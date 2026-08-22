425b51154aabe0f9946a877617ec1ecc9170e229
Damon Chu <dc3800@nyu.edu>
2026-08-22 03:16:34 -0400

    multi-lang setup

--- app/(players)/videojs/page.tsx
+++ app/(players)/videojs/page.tsx
@@ -1,13 +1,15 @@
-import { Suspense } from "react";
-import { MyPlayer } from "@/components/videojsplayer";
+const base = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
 
 export default function VideojsPage() {
   return (
     <>
-      <div className="flex-1 w-full h-full overflow-hidden">
-        <Suspense>
-          <MyPlayer />
-        </Suspense>
+      <div className="flex-1 w-full overflow-hidden">
+        <iframe
+          src={`${base}/videojs/index.html`}
+          className="w-full h-full border-0"
+          title="Video.js Player"
+          allowFullScreen
+        />
       </div>
     </>
   );
